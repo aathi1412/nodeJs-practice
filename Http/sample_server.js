47a5bf828534f25835f8af80dc61c4d8636c8e2b
Aathi <aathithirunav@gmail.com>
2026-08-17 21:54:12 +0530

    feat(http): Add sample HTTP server with video streaming
    - Serves a plain text welcome message on the root path.
    - Streams an MP4 video file on the `/video` path using `fs.createReadStream`.
    - Demonstrates basic HTTP server setup and content serving.

---
+++ Http/sample_server.js
@@ -0,0 +1,21 @@
+import http from "http";
+import fs from "fs";
+
+
+const server =
+    http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.writeHead(200, {"content-type": "text/plain"});
+        res.end("Welcome to Cartoon World");
+    }
+    else if (req.url === "/video") {
+        const readStream = fs.createReadStream("./1.mp4");
+        res.writeHead(200, {"content-type": "video/mp4"});
+        readStream.pipe(res);
+    }
+});
+
+server.listen(3000, () => {
+    console.log("Listening on port 3000");
+    console.log("http://localhost:3000");
+});
