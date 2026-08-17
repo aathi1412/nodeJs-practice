f71a59834da7ffa22750c54e6970d99dcc4fbf20
Aathi <aathithirunav@gmail.com>
2026-08-17 21:54:37 +0530

    feat(fs): Add asynchronous file read/write example
    - Demonstrates reading content from 'text.txt'.
    - Writes the read content to 'res.txt'.
    - Uses asynchronous `fs` module methods.

---
+++ fs.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+
+
+fs.readFile("text.txt", {encoding: "utf8"}, (err, data) => {
+    if (err) throw err;
+    console.log(data);
+
+    fs.writeFile("res.txt", data, (err) => {
+        if (err){
+            console.error(err);
+        }
+        console.log("done");
+    });
+
+});
+
+
+
+console.log("End");
